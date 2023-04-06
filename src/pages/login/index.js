171c1f004bf894643c062c46828da6bdd76a1c1e
namtq184161 <quangnam260800@gmail.com>
2023-04-06 14:16:21 +0700

    feat: login page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,3 +1,46 @@
+import { Form, Formik } from 'formik';
+import { Link } from 'react-router-dom';
+import './style.css';
+
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="login">
+      <div className="login_wrapper">
+        <div className="login_wrap">
+          <div className="login_1">
+            <img src="../../icons/aimer4.png" alt="" />
+            <span>
+              Aimer helps you connect and share with the people in your life.
+            </span>
+          </div>
+          <div className="login_2">
+            <div className="login_2_wrap">
+              <Formik>
+                {(formik) => (
+                  <Form>
+                    <input type="text" />
+                    <input type="text" />
+                    <button className="blue_btn" type="submit">
+                      Log in
+                    </button>
+                  </Form>
+                )}
+              </Formik>
+              <Link className="forgot_password" to="/forgot">
+                Forgotten password?
+              </Link>
+              <div className="sign_splitter"></div>
+              <button className="blue_btn open_signup">
+                Create New Account
+              </button>
+            </div>
+            <Link to="/" className="sign_extra">
+              <b>Create a Page</b> for a celebrity, brand or business.
+            </Link>
+          </div>
+        </div>
+        <div className="register"></div>
+      </div>
+    </div>
+  );
 }
