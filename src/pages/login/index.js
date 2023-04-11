61312772b128536d711922a3dd005cd93282e404
namtq184161 <quangnam260800@gmail.com>
2023-04-11 22:32:59 +0700

    feat: register form header

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,13 +1,14 @@
 import './style.css';
 import LoginForm from '../../components/login/loginForm';
 import Footer from '../../components/login/footer';
+import RegisterForm from '../../components/login/registerForm';
 
 export default function Login() {
   return (
     <div className="login">
       <div className="login_wrapper">
         <LoginForm />
-        <div className="register"></div>
+        <RegisterForm />
         <Footer />
       </div>
     </div>
