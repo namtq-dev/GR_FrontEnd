05748fb91ceed31a3a151d2b58a40f9bbd5f8c77
namtq184161 <quangnam260800@gmail.com>
2023-04-11 20:25:53 +0700

    feat: login page responsive

--- src/components/inputs/loginInput/index.js
+++ src/components/inputs/loginInput/index.js
@@ -1,14 +1,26 @@
 import './style.css';
 import { ErrorMessage, useField } from 'formik';
+import { useMediaQuery } from 'react-responsive';
 
 export default function LoginInput({ bottom, ...params }) {
   const [field, meta] = useField(params);
+  const desktopView = useMediaQuery({
+    query: '(min-width: 850px)',
+  });
+
   return (
     <div className="input_wrap">
       {meta.touched && meta.error && !bottom && (
-        <div className="input_error" style={{ transform: 'translateY(3px)' }}>
+        <div
+          className={
+            desktopView ? 'input_error input_error_desktop' : 'input_error'
+          }
+          style={{ transform: 'translateY(3px)' }}
+        >
           <ErrorMessage name={field.name} />
-          <div className="error_arrow_top"></div>
+          <div
+            className={desktopView ? 'error_arrow_left' : 'error_arrow_top'}
+          ></div>
         </div>
       )}
 
@@ -22,14 +34,24 @@ export default function LoginInput({ bottom, ...params }) {
       />
 
       {meta.touched && meta.error && bottom && (
-        <div className="input_error" style={{ transform: 'translateY(2px)' }}>
+        <div
+          className={
+            desktopView ? 'input_error input_error_desktop' : 'input_error'
+          }
+          style={{ transform: 'translateY(2px)' }}
+        >
           <ErrorMessage name={field.name} />
-          <div className="error_arrow_bottom"></div>
+          <div
+            className={desktopView ? 'error_arrow_left' : 'error_arrow_bottom'}
+          ></div>
         </div>
       )}
 
       {meta.touched && meta.error && (
-        <i className="error_icon" style={{ top: `${!bottom && '63%'}` }}></i>
+        <i
+          className="error_icon"
+          style={{ top: `${!bottom && !desktopView ? '63%' : '15px'}` }}
+        ></i>
       )}
     </div>
   );
