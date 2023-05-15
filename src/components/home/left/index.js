d8e35d1f1ea01080d4f2db6d8d6e8017832ea87d
namtq184161 <quangnam260800@gmail.com>
2023-05-15 21:13:09 +0700

    feat: homepage left bar (cont)

--- src/components/home/left/index.js
+++ src/components/home/left/index.js
@@ -2,10 +2,15 @@ import LeftLink from './leftLink';
 import './style.css';
 import { left } from '../../../data/home';
 import { Link } from 'react-router-dom';
+import { ArrowDown1 } from '../../../svg';
+import { useState } from 'react';
+import Shortcut from './shortcut';
 
 export default function LeftHome({ user }) {
+  const [visible, setVisible] = useState(false);
+
   return (
-    <div className="left_home">
+    <div className="left_home scrollbar">
       <Link to="/profile" className="left_link hover1">
         <img src={user?.picture} alt="" />
         <span>
@@ -20,6 +25,76 @@ export default function LeftHome({ user }) {
           notification={link.notification}
         />
       ))}
+      {!visible && (
+        <div
+          className="left_link hover1"
+          onClick={() => {
+            setVisible(true);
+          }}
+        >
+          <div className="small_circle">
+            <ArrowDown1 />
+          </div>
+          <span>See more</span>
+        </div>
+      )}
+      {visible && (
+        <div className="more_left">
+          {left.slice(8, left.length).map((link, i) => (
+            <LeftLink
+              key={i}
+              img={link.img}
+              text={link.text}
+              notification={link.notification}
+            />
+          ))}
+          <div
+            className="left_link hover1"
+            onClick={() => {
+              setVisible(false);
+            }}
+          >
+            <div className="small_circle rotate180">
+              <ArrowDown1 />
+            </div>
+            <span>Show less</span>
+          </div>
+        </div>
+      )}
+      <div className="splitter"></div>
+      <div className="shortcut">
+        <div className="heading">Your Shortcuts</div>
+        <div className="edit_shortcut">Edit</div>
+      </div>
+      <div className="shortcut_list">
+        <Shortcut
+          link="https://www.youtube.com/"
+          img="../../images/ytb.png"
+          name="Youtube"
+        />
+        <Shortcut
+          link="https://www.instagram.com/"
+          img="../../images/insta.png"
+          name="Instagram"
+        />
+      </div>
+      <div className={`fb_copyright ${visible && 'relative_fb_copyright'}`}>
+        <Link to="/">Privacy </Link>
+        <span>. </span>
+        <Link to="/">Terms </Link>
+        <span>. </span>
+        <Link to="/">Advertising </Link>
+        <span>. </span>
+        <Link to="/">
+          Ad Choices <i className="ad_choices_icon"></i>{' '}
+        </Link>
+        <span>. </span>
+        <Link to="/">Cookies </Link>
+        <span>. </span>
+        <Link to="/">More </Link>
+        <span>. </span> <br />
+        Aimer © 2023
+      </div>
     </div>
   );
 }
