ef0f078e67bf146d866c7b952890ae1535f4b986
namtq184161 <quangnam260800@gmail.com>
2023-05-15 22:47:45 +0700

    feat: homepage right bar

---
+++ src/components/home/right/contact.js
@@ -0,0 +1,12 @@
+export default function Contact({ user }) {
+  return (
+    <div className="contact hover3">
+      <div className="contact_img">
+        <img src={user?.picture} alt="" />
+      </div>
+      <span>
+        {user.firstName} {user.lastName}
+      </span>
+    </div>
+  );
+}
