91f8878c1cb448bdd0c54002633e5de5994ce59a
namtq184161 <quangnam260800@gmail.com>
2023-05-15 22:52:33 +0700

    fix: header style

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -94,7 +94,9 @@ export default function Header() {
               setShowAllMenu((prev) => !prev);
             }}
           >
-            <Menu />
+            <div style={{ transform: 'translateY(2px)' }}>
+              <Menu />
+            </div>
           </div>
           {showAllMenu && <AllMenu />}
         </div>
@@ -114,7 +116,9 @@ export default function Header() {
               setShowUserMenu((prev) => !prev);
             }}
           >
-            <ArrowDown />
+            <div style={{ transform: 'translateY(2px)' }}>
+              <ArrowDown />
+            </div>
           </div>
           {showUserMenu && <UserMenu user={user} />}
         </div>
