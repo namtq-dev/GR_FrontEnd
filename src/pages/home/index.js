0c0d9601b6e2c67702ae07de0de8b28668ac54de
namtq184161 <quangnam260800@gmail.com>
2023-05-16 23:33:48 +0700

    feat: create post component

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux';
 import RightHome from '../../components/home/right';
 import Stories from '../../components/home/stories';
 import './style.css';
+import CreatePost from '../../components/createPost';
 
 export default function Home() {
   const { user } = useSelector((state) => ({ ...state }));
@@ -14,6 +15,7 @@ export default function Home() {
       <LeftHome user={user} />
       <div className="home_middle">
         <Stories />
+        <CreatePost user={user} />
       </div>
       <RightHome user={user} />
     </div>
