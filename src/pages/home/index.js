55953e5c29252fb668895765c1ea10a6d06eb8d3
namtq184161 <quangnam260800@gmail.com>
2023-05-18 20:02:01 +0700

    feat: homepage responsive

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,8 +3,8 @@ import LeftHome from '../../components/home/left';
 import { useSelector } from 'react-redux';
 import RightHome from '../../components/home/right';
 import Stories from '../../components/home/stories';
-import './style.css';
 import CreatePost from '../../components/createPost';
+import './style.css';
 
 export default function Home() {
   const { user } = useSelector((state) => ({ ...state }));
