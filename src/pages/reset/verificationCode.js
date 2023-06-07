9fb0edc464726bd1392b6b4d8eea904e75f131c8
namtq184161 <quangnam260800@gmail.com>
2023-06-07 21:08:55 +0700

    feat: verification code form validate

--- src/pages/reset/verificationCode.js
+++ src/pages/reset/verificationCode.js
@@ -1,15 +1,27 @@
 import { Form, Formik } from 'formik';
 import LoginInput from '../../components/inputs/loginInput';
 import { Link } from 'react-router-dom';
+import * as Yup from 'yup';
 
 export default function VerificationCode({ code, setCode, error }) {
+  const validateCode = Yup.object({
+    code: Yup.string()
+      .required('Verification code is required')
+      .min('5', 'Invalid verification code')
+      .max('5', 'Invalid verification code'),
+  });
+
   return (
     <div className="reset_form">
       <div className="reset_form_header">Verification Code</div>
       <div className="reset_form_text">
         Please enter the code that has been sent to your email.
       </div>
-      <Formik enableReinitialize initialValues={{ code }}>
+      <Formik
+        enableReinitialize
+        initialValues={{ code }}
+        validationSchema={validateCode}
+      >
         {(formik) => (
           <Form>
             <LoginInput
