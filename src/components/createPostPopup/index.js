ddabc0c9af341842006eba6fb106ac26e6b0f816
namtq184161 <quangnam260800@gmail.com>
2023-06-16 15:06:24 +0700

    feat: post emoji picker handler

--- src/components/createPostPopup/index.js
+++ src/components/createPostPopup/index.js
@@ -1,9 +1,29 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import Picker from 'emoji-picker-react';
 import './style.css';
 
 export default function CreatePostPopup({ user }) {
   const [text, setText] = useState('');
   const [showPrev, setShowPrev] = useState(false);
+  const [picker, setPicker] = useState(false);
+  const [cursorPosition, setCursorPosition] = useState();
+
+  const textRef = useRef(null);
+
+  useEffect(() => {
+    textRef.current.selectionEnd = cursorPosition;
+  }, [cursorPosition]);
+
+  const handleEmoji = (eve, { emoji }) => {
+    const ref = textRef.current;
+    ref.focus();
+    const textBeforeCursor = text.substring(0, ref.selectionStart);
+    const textAfterCursor = text.substring(ref.selectionStart);
+    const newText = textBeforeCursor + emoji + textAfterCursor;
+
+    setText(newText);
+    setCursorPosition(textBeforeCursor.length + emoji.length);
+  };
 
   return (
     <div className="blur">
@@ -15,10 +35,10 @@ export default function CreatePostPopup({ user }) {
           <span>Create Post</span>
         </div>
         <div className="box_profile">
-          <img src={user.picture} alt="" className="box_profile_img" />
+          <img src={user?.picture} alt="" className="box_profile_img" />
           <div className="box_col">
             <div className="box_profile_name">
-              {user.firstName} {user.lastName}
+              {user?.firstName} {user?.lastName}
             </div>
             <div className="box_privacy">
               <img src="../../../icons/public.png" alt="" />
@@ -30,14 +50,29 @@ export default function CreatePostPopup({ user }) {
         {!showPrev && (
           <div className="flex_center">
             <textarea
-              maxlength="500"
+              ref={textRef}
+              maxLength="500"
               value={text}
-              placeholder={`What's on your mind, ${user.firstName}?`}
+              placeholder={`What's on your mind, ${user?.firstName}?`}
               className="post_input"
               onChange={(eve) => setText(eve.target.value)}
             ></textarea>
           </div>
         )}
+        <div className="post_emojis_wrap">
+          {picker && (
+            <div className="comment_emoji_picker rlmove">
+              <Picker onEmojiClick={handleEmoji} />
+            </div>
+          )}
+          <img src="../../../icons/colorful.png" alt="" />
+          <i
+            className="emoji_icon_large"
+            onClick={() => {
+              setPicker((prev) => !prev);
+            }}
+          ></i>
+        </div>
       </div>
     </div>
   );
