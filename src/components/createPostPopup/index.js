247fe1977761470e7ee30bda1a62e54a6e4e9104
namtq184161 <quangnam260800@gmail.com>
2023-07-07 18:32:59 +0700

    feat: create post with background, text and emoji

--- src/components/createPostPopup/index.js
+++ src/components/createPostPopup/index.js
@@ -4,18 +4,32 @@ import AddToYourPost from './addToYourPost';
 import ImagePreview from './imagePreview';
 import './style.css';
 import useClickOutside from '../../helpers/clickOutside';
+import { createPost } from '../../helpers/post';
+import PulseLoader from 'react-spinners/PulseLoader';
 
 export default function CreatePostPopup({ user, setCreatePostVisible }) {
   const [text, setText] = useState('');
   const [showPrev, setShowPrev] = useState(false);
   const [images, setImages] = useState([]);
   const [background, setBackground] = useState('');
+  const [loading, setLoading] = useState(false);
 
   const createPostPopup = useRef(null);
 
   useClickOutside(createPostPopup, () => {
     setCreatePostVisible(false);
   });
+
+  const postSubmit = async () => {
+    if (background) {
+      setLoading(true);
+      await createPost(null, background, text, null, user.id, user.loginToken);
+      setLoading(false);
+      setBackground('');
+      setText('');
+      setCreatePostVisible(false);
+    }
+  };
 
   return (
     <div className="blur">
@@ -65,7 +79,15 @@ export default function CreatePostPopup({ user, setCreatePostVisible }) {
           />
         )}
         <AddToYourPost setShowPrev={setShowPrev} />
-        <button className="post_submit">Post</button>
+        <button
+          className="post_submit"
+          onClick={() => {
+            postSubmit();
+          }}
+          disabled={loading}
+        >
+          {loading ? <PulseLoader color="#fff" size={5} /> : 'Post'}
+        </button>
       </div>
     </div>
   );
