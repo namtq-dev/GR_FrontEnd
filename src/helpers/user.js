f107fcbb28bb12153f6045796b32cc82976f2614
namtq184161 <quangnam260800@gmail.com>
2023-07-15 09:03:01 +0700

    feat: request function to backend

--- src/helpers/user.js
+++ src/helpers/user.js
@@ -35,3 +35,136 @@ export const updateCoverPicture = async (url, token) => {
     return error.response.data.message;
   }
 };
+
+export const addFriend = async (id, token) => {
+  try {
+    const { data } = await axios.patch(
+      `${process.env.REACT_APP_BACKEND_URL}/addFriend/${id}`,
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+
+    if (
+      data.response.message === 'Your friend request has been sent successfully'
+    ) {
+      return 'OK';
+    }
+  } catch (error) {
+    return error.response.data.message;
+  }
+};
+
+export const cancelFriendRequest = async (id, token) => {
+  try {
+    const { data } = await axios.delete(
+      `${process.env.REACT_APP_BACKEND_URL}/cancelFriendRequest/${id}`,
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+
+    if (
+      data.response.message === 'Your cancel request has been sent successfully'
+    ) {
+      return 'OK';
+    }
+  } catch (error) {
+    return error.response.data.message;
+  }
+};
+
+export const follow = async (id, token) => {
+  try {
+    const { data } = await axios.patch(
+      `${process.env.REACT_APP_BACKEND_URL}/follow/${id}`,
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+
+    if (
+      data.response.message === 'Your follow request has been sent successfully'
+    ) {
+      return 'OK';
+    }
+  } catch (error) {
+    return error.response.data.message;
+  }
+};
+
+export const unfollow = async (id, token) => {
+  try {
+    const { data } = await axios.patch(
+      `${process.env.REACT_APP_BACKEND_URL}/unfollow/${id}`,
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+
+    if (
+      data.response.message ===
+      'Your unfollow request has been sent successfully'
+    ) {
+      return 'OK';
+    }
+  } catch (error) {
+    return error.response.data.message;
+  }
+};
+
+export const acceptFriend = async (id, token) => {
+  try {
+    const { data } = await axios.patch(
+      `${process.env.REACT_APP_BACKEND_URL}/acceptFriend/${id}`,
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+
+    if (
+      data.response.message ===
+      'You have successfully become friend with this user'
+    ) {
+      return 'OK';
+    }
+  } catch (error) {
+    return error.response.data.message;
+  }
+};
+
+export const unfriend = async (id, token) => {
+  try {
+    const { data } = await axios.delete(
+      `${process.env.REACT_APP_BACKEND_URL}/unfriend/${id}`,
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+
+    if (data.response.message === 'You have successfully unfriend this user') {
+      return 'OK';
+    }
+  } catch (error) {
+    return error.response.data.message;
+  }
+};
+
+export const deleteFriendRequest = async (id, token) => {
+  try {
+    const { data } = await axios.delete(
+      `${process.env.REACT_APP_BACKEND_URL}/deleteFriendRequest/${id}`,
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+
+    if (
+      data.response.message === 'You have successfully decline a friend request'
+    ) {
+      return 'OK';
+    }
+  } catch (error) {
+    return error.response.data.message;
+  }
+};
