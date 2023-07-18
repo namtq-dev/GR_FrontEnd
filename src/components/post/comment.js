ba7b9a4b9c3ebf93b2653fe0057415619fbaa1cc
namtq184161 <quangnam260800@gmail.com>
2023-07-18 18:55:52 +0700

    feat: hide negative comments

--- src/components/post/comment.js
+++ src/components/post/comment.js
@@ -1,6 +1,15 @@
+import { useRef } from 'react';
 import Moment from 'react-moment';
 
 export default function Comment({ comment }) {
+  const commentTextRef = useRef(null);
+  const unhideButtonRef = useRef(null);
+
+  const unhideComment = () => {
+    commentTextRef.current.innerText = comment.comment;
+    unhideButtonRef.current.style.display = 'None';
+  };
+
   return (
     <div className="comment">
       <img
@@ -13,7 +22,11 @@ export default function Comment({ comment }) {
           <div className="comment_name">
             {comment.commentBy.firstName} {comment.commentBy.lastName}
           </div>
-          <div className="comment_text">{comment.comment}</div>
+          <div className="comment_text" ref={commentTextRef}>
+            {Number(comment.score) <= -1
+              ? 'This comment is hidden.'
+              : comment.comment}
+          </div>
         </div>
         {comment.image && (
           <img src={comment.image} alt="" className="comment_image" />
@@ -21,6 +34,11 @@ export default function Comment({ comment }) {
         <div className="comment_actions">
           <span>Like</span>
           <span>Reply</span>
+          {Number(comment.score) <= -1 && (
+            <span ref={unhideButtonRef} onClick={() => unhideComment()}>
+              Unhide
+            </span>
+          )}
           <span>
             <Moment fromNow interval={60}>
               {comment.commentAt}
