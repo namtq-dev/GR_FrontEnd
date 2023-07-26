1d28a161bc67b6fe841a959b1f6281edd932b20d
namtq184161 <quangnam260800@gmail.com>
2023-07-26 19:55:37 +0700

    chore: show friends contact in side bar

--- src/components/home/right/contact.js
+++ src/components/home/right/contact.js
@@ -1,12 +1,24 @@
 export default function Contact({ user }) {
   return (
-    <div className="contact hover3">
-      <div className="contact_img">
-        <img src={user?.picture} alt="" />
+    <>
+      <div className="contact hover3">
+        <div className="contact_img">
+          <img src={user?.picture} alt="" />
+        </div>
+        <span>
+          {user.firstName} {user.lastName}
+        </span>
       </div>
-      <span>
-        {user.firstName} {user.lastName}
-      </span>
-    </div>
+      {user?.friends?.map((friend, i) => (
+        <div className="contact hover3" key={i}>
+          <div className="contact_img">
+            <img src={friend?.picture} alt="" />
+          </div>
+          <span>
+            {friend.firstName} {friend.lastName}
+          </span>
+        </div>
+      ))}
+    </>
   );
 }
